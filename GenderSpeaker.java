f6b723ba543c5006e1f8ec33ffa1ecf10168a16e
hamuichiro <hamuichiro8616@icloud.com>
2022-02-15 16:05:43 +0900

    #1

--- GenderSpeaker.java
+++ GenderSpeaker.java
@@ -1,13 +1,14 @@
 public class GenderSpeaker {
     public static void main(String[] args) {
-        if (args.equals("’j«")) {
-            Man man = new Man();
-            man.speak();
-        } else { 
-            Woman woman = new Woman();
-            woman.speak();
+        if (args.length != 0) {
+            if (args.equals("’j«")) {
+                Man man = new Man();
+                man.speak();
+            } else { 
+                Woman woman = new Woman();
+                woman.speak();
+            }
         }
-        
         
     }
 }
